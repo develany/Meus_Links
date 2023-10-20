91d0e8a9ca74acee872e9bcf5a0aef8fb1f9aed0
Elany Peixoto <elanysantos5@outlook.com>
2023-10-20 09:06:46 -0300

    solucao

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import './App.css';
 import Routes from './Routes';
 import { isLogado } from './auth';
 import Menu from './components/Menu/Menu';
-import Configuracoes from './pages/Cofiguracoes/Configuracoes';
 
 function App() {
   return (
     <div className="bg-primary text-white p-3 text-center mx-auto"
       style={{ width: '50%', marginTop: "3%" }}>
-        {isLogado()&&<Menu />}
+      {isLogado() && <Menu />}
       <Routes />
     </div>
   );
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,26 +1,24 @@
-import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import Offcanvas from 'react-bootstrap/Offcanvas';
+import { Link } from 'react-router-dom';
 
 
 
 function Menu() {
-    const deslogar = () => {
-        localStorage.removeItem('logado');
-        localStorage.removeItem('id');
-        localStorage.removeItem('token');
-        window.location.href = `/login`;
-      }
+  const deslogar = () => {
+    localStorage.removeItem('logado');
+    localStorage.removeItem('id');
+    localStorage.removeItem('token');
+    window.location.href = `/login`;
+  }
   return (
     <>
       {['xxl'].map((expand) => (
         <Navbar key={expand} expand={expand}>
           <Container fluid>
-        
+
             <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
             <Navbar.Offcanvas
               id={`offcanvasNavbar-expand-${expand}`}
@@ -34,9 +32,9 @@ function Menu() {
               </Offcanvas.Header>
               <Offcanvas.Body>
                 <Nav className="justify-content-end flex-grow-1 pe-3">
-                  <Nav.Link href="http://localhost:3000/usuarios">Usuarios</Nav.Link>
-                  <Nav.Link href='http://localhost:3000/configuracoes'>Configurações</Nav.Link>
-                  <Nav.Link onClick={deslogar}>Sair</Nav.Link>
+                  <Link to="/usuarios">Usuarios</Link>
+                  <Link to="/configuracoes">Configurações</Link>
+                  <Link onClick={deslogar}>Sair</Link>
                 </Nav>
               </Offcanvas.Body>
             </Navbar.Offcanvas>
